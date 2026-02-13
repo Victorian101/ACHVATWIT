6738cded05cb7159eca886bc3e16391e7c57bdcb
Victorian <victorianbelayev@gmail.com>
2026-02-13 14:42:44 +0200

    fix flyway

--- social-media-db/src/main/java/com/college/utils/DbUtils.java
+++ social-media-db/src/main/java/com/college/utils/DbUtils.java
@@ -8,24 +8,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Component
 public class DbUtils {
+    @Autowired
+    private DataSource dataSource;
+
     private Connection connection;
 
     @PostConstruct
     public void init() {
         try {
-            String host = "localhost";
-            String username = "root";
-            String password = "12345678";
-            String schema = "project_2025";
-            int port = 3306;
-            String url = "jdbc:mysql://"
-                    + host + ":" + port + "/"
-                    + schema;
-            this.connection =
-                    DriverManager.getConnection(url, username, password);
+            this.connection = dataSource.getConnection();
             System.out.println("Connection established");
         } catch (SQLException e) {
             System.out.println("Failed to create db connection");
--- social-media-db/src/main/resources/db/migration/V1__init.sql
+++ social-media-db/src/main/resources/db/migration/V1__init.sql
@@ -1,56 +1,52 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(64) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    url VARCHAR(1024) DEFAULT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY uk_users_username (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS users
+(
+    username          VARCHAR(64)  NOT NULL,
+    password          VARCHAR(255) NOT NULL,
+    profile_image_url VARCHAR(1024) DEFAULT NULL,
+    PRIMARY KEY (username)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS posts (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    user_id BIGINT NOT NULL,
-    username VARCHAR(64) NOT NULL,
-    content VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS posts
+(
+    id       INT          NOT NULL AUTO_INCREMENT,
+    username VARCHAR(64)  NOT NULL,
+    content  VARCHAR(500) NOT NULL,
     PRIMARY KEY (id),
-    KEY idx_posts_user_id (user_id),
     KEY idx_posts_username (username),
     CONSTRAINT fk_posts_user
-        FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        FOREIGN KEY (username) REFERENCES users (username)
+            ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS follows (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    follower_id BIGINT NOT NULL,
-    followed_id BIGINT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY uk_follows_pair (follower_id, followed_id),
-    KEY idx_follows_follower (follower_id),
-    KEY idx_follows_followed (followed_id),
+CREATE TABLE IF NOT EXISTS follows
+(
+    follower_username VARCHAR(64) NOT NULL,
+    followed_username VARCHAR(64) NOT NULL,
+    PRIMARY KEY (follower_username, followed_username),
+    KEY idx_follows_followed (followed_username),
     CONSTRAINT fk_follows_follower
-        FOREIGN KEY (follower_id) REFERENCES users(id)
-        ON DELETE CASCADE,
+        FOREIGN KEY (follower_username) REFERENCES users (username)
+            ON DELETE CASCADE,
     CONSTRAINT fk_follows_followed
-        FOREIGN KEY (followed_id) REFERENCES users(id)
-        ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        FOREIGN KEY (followed_username) REFERENCES users (username)
+            ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS post_reactions (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    post_id BIGINT NOT NULL,
-    user_id BIGINT NOT NULL,
-    reaction TINYINT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY uk_post_reaction_user (post_id, user_id),
-    KEY idx_post_reactions_post (post_id),
-    KEY idx_post_reactions_user (user_id),
+CREATE TABLE IF NOT EXISTS post_reactions
+(
+    post_id  INT         NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    reaction TINYINT     NOT NULL,
+    PRIMARY KEY (post_id, username),
+    KEY idx_post_reactions_username (username),
     CONSTRAINT fk_post_reactions_post
-        FOREIGN KEY (post_id) REFERENCES posts(id)
-        ON DELETE CASCADE,
+        FOREIGN KEY (post_id) REFERENCES posts (id)
+            ON DELETE CASCADE,
     CONSTRAINT fk_post_reactions_user
-        FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE,
-    CONSTRAINT chk_reaction
-        CHECK (reaction IN (-1, 0, 1))
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        FOREIGN KEY (username) REFERENCES users (username)
+            ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
